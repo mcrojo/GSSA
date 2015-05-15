c1c5f106109a179bb84a1d49ab65317ee94cef4a
Cony <constanzarj@gmail.com>
2015-05-15 18:34:50 -0500

    Vignette added

--- R/gsoptim.R
+++ R/gsoptim.R
@@ -1,7 +1,7 @@
 gsoptim<-function(g,int,e=1e-6){
   
   # Check parameters
-  if(length(int)!=2) stop("The interval needs to be in R2")
+  if(length(int)!=2) stop("The length of the interval must be 2")
   if(e<=0) stop("Tolerance needs to be positive")
   if(!is.function(g)) stop("g needs to be a function")
   if(length(formals(g))!=1) stop("g needs to be a function that takes only one element as an argument")
--- R/gssMethods.R
+++ R/gssMethods.R
@@ -1,77 +1,21 @@
-
-setMethod(f = "[", signature = "gss", definition=function(x, i, drop="missing"){
-            
-	if(i == "min"){return(x@min)}else{}
-	if(i == "int.lower"){return(x@int.lower)}else{}
-	if(i == "int.upper"){return(x@int.upper)}else{}
-	if(i == "it"){return(x@it)}else{}
-	if(i == "g"){return(x@g)}else{}
-    
-})
-
-
-setMethod(f = "[[", signature = "gss", definition=function(x, i, drop="missing"){
-
-	if(i == "min"){return(x@min)}else{}
-	if(i == "int.lower"){return(x@int.lower)}else{}
-	if(i == "int.upper"){return(x@int.upper)}else{}
-	if(i == "it"){return(x@it)}else{}
-	if(i == "g"){return(x@g)}else{}
-                     
-})
-
-
 ## PRINT
-setMethod("show", "gss",function(object){
+setMethod(f="show", signature ="gss",definition = function(object){
 
 	cat("The minimum is:\n")
 	print(object@min)
        
 })
 
-
 ## PLOT
 setMethod(f="plot", signature = "gss",  definition = function(x, y, ...){
   
 	g<-as.function(x@g)
 	sequence<-seq(x@int.lower,x@int.upper,length.out=1000)
-	plot(sequence,g(sequence), main="Minimum by Golden Section Search", ylab=expression(g(x)), xlab=expression(x), type="l",...)
+	plot(sequence,g(sequence), ylab=expression(g(x)), xlab=expression(x), type="l",...)
 	points(x@min,g(x@min), lwd=2, col="red", pch=18)
 
 })
           
-          
-
-
-
-
-setReplaceMethod(f = "[", signature = "gss", definition=function(x,i,value){
-
-	if(i == "min"){return(x@min)}else{}
-	if(i == "int.lower"){return(x@int.lower)}else{}
-	if(i == "int.upper"){return(x@int.upper)}else{}
-	if(i == "it"){return(x@it)}else{}
-	if(i == "g"){return(x@g)}else{}
-	return(x)
-
-})
-
-
-setReplaceMethod(f = "[[", signature = "gss", definition=function(x,i,value){
-  
-
-	if(i == "min"){return(x@min)}else{}
-	if(i == "int.lower"){return(x@int.lower)}else{}
-	if(i == "int.upper"){return(x@int.upper)}else{}
-	if(i == "it"){return(x@it)}else{}
-	if(i == "g"){return(x@g)}else{}
-	return(x)
-
-
-
-})
-
-
 ## SUMMARY
 setMethod(f="summary", signature = "gss",  definition = function(object){
 
